ca1f8b70cf14ea2ae6f99c3bdb793b8cc31cf6a9
Bahdtoluwani <oladosu.toluene@gmail.com>
2024-05-08 16:46:50 +0100

    little fix

--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -10,7 +10,7 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 
 builder.Services.AddDbContext<AppDbContext>(option =>
 {
